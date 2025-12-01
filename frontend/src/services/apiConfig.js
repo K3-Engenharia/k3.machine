985dcdb44f6636f6c3060e70ef92655a1c763813
K3-Engenharia <kevin.k3engenharia@gmail.com>
2025-11-30 23:41:24 -0400

    Atualizado para Render

--- frontend/src/services/apiConfig.js
+++ frontend/src/services/apiConfig.js
@@ -1,3 +1,3 @@
 // src/services/apiConfig.js
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+const API_URL = process.env.REACT_APP_API_URL || (typeof window !== 'undefined' && window.location.hostname === 'localhost' ? 'http://localhost:3001' : 'https://k3-machine-ln9c.onrender.com');
 export default API_URL;
